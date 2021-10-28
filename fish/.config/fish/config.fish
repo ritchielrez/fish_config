82e10089a783ad7a1a90172cf965308d82a25a01
RitchielRez <ritchielrez@gmail.com>
2021-10-28 09:36:30 +0600

    Added a new path for rubygems

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 # prompt
-starship init fish | source
+oh-my-posh --init --shell fish --config ~/.poshthemes/zash.omp.json | source
 
 # path variable
 source ~/.config/fish/path.fish
--- fish/.config/fish/path.fish
+++ fish/.config/fish/path.fish
@@ -1,1 +1,2 @@
 export PATH="$HOME/.local/bin/:$PATH"
+export PATH="/usr/lib/ruby/gems/3.0.0/bin/:$PATH"
